579abd805a63a6e5533d7383da96b07b7b5603e5
aibeam <aibeam1025@gmail.com>
2022-03-05 23:02:21 +0900

    댓글 토글 script

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+function cntText(){
+  var textLength = document.getElementById("text-area").value.length;
+  document.getElementById("cnt-text").innerHTML = textLength;
+}
+
 function addImg(){
   var fileName = document.getElementById("add-img").value;
   document.getElementById("img-name").innerHTML = fileName;
+}
+
+function replyToggle(){
+  var reply = document.querySelector(".reply-section");
+  var margin = document.querySelector(".feed-info");
+  if(reply.style.display === "none" || reply.style.display === ""){
+    reply.style.display = "block";
+    margin.style.paddingBottom = "15px";
+  } else {
+    reply.style.display = "none";
+    margin.style.paddingBottom = "40px";
+  }
 }
